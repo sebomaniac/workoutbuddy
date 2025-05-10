2492359218303bfd2f27182b159934ef4b46a7b8
Lavender Hwang <lhwang914@gmail.com>
2025-05-09 17:42:26 -0700

    Setup pages folder and created basic routes

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Welcome back</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
